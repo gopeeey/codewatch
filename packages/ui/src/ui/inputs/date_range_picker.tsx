b9062ee209a1471d4949e5f208e08b4690bb3a53
Samuel Gopeh <sammygopeh@gmail.com>
2024-04-24 00:22:42 +0100

    created basic calendar

--- packages/ui/src/ui/inputs/date_range_picker.tsx
+++ packages/ui/src/ui/inputs/date_range_picker.tsx
@@ -1,15 +1,32 @@
 import useClickAwayListener from "@hooks/use_click_away_listener";
 import { Card } from "@ui/card";
 import clsx from "clsx";
-import { useCallback, useEffect, useRef } from "react";
+import moment, { Moment } from "moment";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 type Props = {
   anchorId: string;
   open: boolean;
   onClose: () => void;
 };
+
 export function DateRangePicker({ anchorId, open, onClose }: Props) {
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const [focusDate, setFocusDate] = useState(moment());
+
+  const dateRange = useMemo(() => {
+    const start = focusDate.clone().startOf("month").startOf("week");
+    const end = focusDate.clone().endOf("month").endOf("week");
+    const dates: Moment[] = [];
+
+    while (!start.isAfter(end, "day")) {
+      dates.push(start.clone());
+      start.add(1, "day");
+    }
+
+    return dates;
+  }, [focusDate]);
+
   const handleClose = useCallback(() => {
     if (!open) return;
     onClose();
@@ -36,7 +53,28 @@ export function DateRangePicker({ anchorId, open, onClose }: Props) {
         { "!opacity-100 !scale-y-100": open }
       )}
     >
-      Hello world
+      {/* Calendar */}
+      <div className="grid grid-cols-7 grid-flow-row gap-y-4">
+        {moment.weekdaysMin().map((day) => (
+          <div
+            key={day}
+            className="py-1 px-3 text-sm text-center text-grey-700"
+          >
+            {day}
+          </div>
+        ))}
+
+        {dateRange.map((date) => (
+          <div
+            key={date.toISOString()}
+            className={clsx("py-1 px-3 text-sm text-center cursor-pointer", {
+              "text-grey-900": !date.isSame(focusDate, "month"),
+            })}
+          >
+            {date.get("date")}
+          </div>
+        ))}
+      </div>
     </Card>
   );
 }
