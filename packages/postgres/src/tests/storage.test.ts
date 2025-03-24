68e2697f76d1d220609599c89711d698f7e5883a
Samuel Gopeh <sammygopeh@gmail.com>
2025-03-24 21:34:31 +0100

    updated to core 1.0.7

--- packages/postgres/src/tests/storage.test.ts
+++ packages/postgres/src/tests/storage.test.ts
@@ -1,20 +1,11 @@
 import { StorageTester } from "codewatch-core/dist/storage";
 import { Issue, Occurrence, Transaction } from "codewatch-core/dist/types";
 import SQL from "sql-template-strings";
-import { CodewatchPgStorage, PgTransaction } from "../storage";
+import { PgTransaction } from "../storage";
 import { DbIssue } from "../types";
-import { setup } from "./utils";
+import { generateBulkInsert, getStorage, setup } from "./utils";
 
-const getStorage = () => {
-  const storage = new CodewatchPgStorage({
-    user: process.env.POSTGRES_DB_USERNAME,
-    host: process.env.POSTGRES_DB_HOST,
-    database: process.env.POSTGRES_DB_NAME,
-    password: process.env.POSTGRES_DB_PASSWORD,
-    port: Number(process.env.POSTGRES_DB_PORT),
-  });
-  return storage;
-};
+const storage = getStorage();
 
 const dbIssueToIssue = (dbIssue: DbIssue) => {
   return {
@@ -23,7 +14,7 @@ const dbIssueToIssue = (dbIssue: DbIssue) => {
   };
 };
 
-const storageTester = new StorageTester(getStorage);
+const storageTester = new StorageTester(storage);
 
 const pool = setup(storageTester);
 
@@ -49,6 +40,14 @@ async function getMultipleIssuesByIds(ids: Issue["id"][]): Promise<Issue[]> {
 
   return resolvedIssues.map(dbIssueToIssue);
 }
+
+storageTester.init.change_ready_state_to_true.setSeedFunc(async () => {
+  return getStorage();
+});
+
+storageTester.close.change_ready_state_to_false.setSeedFunc(async () => {
+  return getStorage();
+});
 
 storageTester.createIssue.persist_issue.setPostProcessingFunc(
   async ({ id, transaction }) => {
@@ -113,44 +112,22 @@ storageTester.runInTransaction.call_back_doesnt_throw_error.commit_transaction.s
   }
 );
 
-storageTester.seededCrud.setInsertTestIssueFn(async (data) => {
-  const query = SQL`INSERT INTO codewatch_pg_issues ( "`;
-  const keys: string[] = [];
-  const values: string[] = [];
-  for (const entry of Object.entries(data)) {
-    keys.push(entry[0]);
-    values.push(entry[1]);
+storageTester.seededCrud.setInsertIssuesFn(async (data) => {
+  const query = generateBulkInsert("codewatch_pg_issues", data, "*");
+
+  try {
+    const { rows } = await pool.query<DbIssue>(query);
+    if (!rows.length) throw new Error("Failed to insert issue");
+    return rows.map(dbIssueToIssue);
+  } catch (err) {
+    console.log(query.sql);
+    throw err;
   }
-  query.append(keys.join(`", "`)).append(`") VALUES (`);
-  values.forEach((value, index) => {
-    if (index === values.length - 1) {
-      query.append(SQL`${value}) `);
-    } else {
-      query.append(SQL`${value}, `);
-    }
-  });
-
-  query.append("RETURNING id;");
-
-  const { rows } = await pool.query<Pick<DbIssue, "id">>(query);
-  if (!rows.length) throw new Error("Failed to insert issue");
-  return rows[0].id.toString();
 });
 
-storageTester.seededCrud.setInsertTestOccurrenceFn(async (data) => {
-  const occurrenceQuery = SQL`
-      INSERT INTO codewatch_pg_occurrences (
-        "issueId", "message", "stderrLogs", "stdoutLogs", "timestamp", "stack"
-      ) VALUES (
-        ${data.issueId},
-        ${data.message},
-        ${data.stderrLogs},
-        ${data.stdoutLogs},
-        ${data.timestamp},
-        ${data.stack}
-      );`;
-
-  await pool.query(occurrenceQuery);
+storageTester.seededCrud.setInsertOccurrencesFn(async (data) => {
+  const query = generateBulkInsert("codewatch_pg_occurrences", data);
+  await pool.query(query);
 });
 
 storageTester.seededCrud.delete_issues.delete_issues_with_supplied_ids.setSeedFunc(
--- packages/postgres/src/tests/utils.ts
+++ packages/postgres/src/tests/utils.ts
@@ -1,49 +1,10 @@
 import { StorageTester } from "codewatch-core/dist/storage";
-import { Issue } from "codewatch-core/dist/types";
 import { config } from "dotenv";
-import Levenshtein from "levenshtein";
 import pg from "pg";
 import SQL from "sql-template-strings";
 import { CodewatchPgStorage } from "../storage";
-import { DbIssue } from "../types";
 
 config();
-
-export const dbSetup = () => {
-  // Connect to the database
-  const pool = new pg.Pool({
-    user: process.env.POSTGRES_DB_USERNAME,
-    host: process.env.POSTGRES_DB_HOST,
-    database: process.env.POSTGRES_DB_NAME,
-    password: process.env.POSTGRES_DB_PASSWORD,
-    port: Number(process.env.POSTGRES_DB_PORT),
-  });
-
-  afterEach(async () => {
-    // Truncate each table except migrations
-    await pool.query(SQL`TRUNCATE codewatch_pg_issues CASCADE;`);
-    await pool.query(
-      SQL`ALTER SEQUENCE codewatch_pg_issues_id_seq RESTART WITH 1;`
-    );
-    await pool.query(
-      SQL`ALTER SEQUENCE codewatch_pg_occurrences_id_seq RESTART WITH 1;`
-    );
-    await pool.query(SQL`TRUNCATE codewatch_pg_occurrences CASCADE;`);
-  }, 5000);
-
-  afterAll(async () => {
-    await pool.query(`
-    DROP SCHEMA public CASCADE;
-    CREATE SCHEMA public;
-    GRANT ALL ON SCHEMA public TO postgres;
-    GRANT ALL ON SCHEMA public TO public;
-    COMMENT ON SCHEMA public IS 'standard public schema';
-    `);
-    pool.end();
-  }, 5000);
-
-  return pool;
-};
 
 export const setup = (tester: StorageTester) => {
   // Connect to the database
@@ -85,76 +46,56 @@ export const setup = (tester: StorageTester) => {
   return pool;
 };
 
-export interface CreateIssueData extends Omit<Issue, "id" | "resolved"> {
-  resolved?: Issue["resolved"];
-}
-export const createCreateIssueData = (
-  timestamp: string,
-  overrides?: Partial<Omit<Issue, "id">>
+export const generateBulkInsert = (
+  tableName: string,
+  data: Record<any, any>[],
+  returning?: string
 ) => {
-  const issue: CreateIssueData = {
-    fingerprint: "123456789012345678",
-    lastOccurrenceTimestamp: timestamp,
-    createdAt: timestamp,
-    lastOccurrenceMessage: "",
-    archived: false,
-    totalOccurrences: 1,
-    unhandled: false,
-    name: "Error 1",
-    isLog: false,
-    ...overrides,
-  };
-  return issue;
-};
-
-export const insertTestIssue = async (pool: pg.Pool, data: CreateIssueData) => {
-  const query = SQL`INSERT INTO codewatch_pg_issues ( "`;
-  const keys: string[] = [];
-  const values: string[] = [];
-  for (const entry of Object.entries(data)) {
-    keys.push(entry[0]);
-    values.push(entry[1]);
-  }
-  query.append(keys.join(`", "`)).append(`") VALUES (`);
-  values.forEach((value, index) => {
-    if (index === values.length - 1) {
-      query.append(SQL`${value}) `);
-    } else {
-      query.append(SQL`${value}, `);
-    }
+  const query = SQL`INSERT INTO `;
+  query.append(tableName);
+  query.append(` ( "`);
+  const keySet = new Set<string>();
+  data.forEach((row) => {
+    Object.keys(row).forEach((key) => {
+      keySet.add(key);
+    });
   });
 
-  query.append("RETURNING id;");
+  const keys = [...keySet];
+  query.append(keys.join(`", "`)).append(`") VALUES `);
 
-  const { rows } = await pool.query<Pick<DbIssue, "id">>(query);
+  for (let i = 0; i < data.length; i++) {
+    const valueQuery = SQL`(`;
+    const values = keys.map((key) => {
+      if (key === "resolved") return Boolean(data[i][key]);
+      return data[i][key];
+    });
 
-  for (let i = 0; i < data.totalOccurrences; i++) {
-    const occurrenceQuery = SQL`
-    INSERT INTO codewatch_pg_occurrences (
-      "issueId", "message", "stderrLogs", "stdoutLogs", "timestamp", "stack"
-    ) VALUES (
-      ${rows[0].id},
-      ${`Occurrence for ${data.name}`},
-      ${[]},
-      ${[]},
-      ${new Date(
-        i + 1 === data.totalOccurrences
-          ? data.lastOccurrenceTimestamp
-          : data.createdAt
-      )},
-      ${`Occurrence stack for ${data.name}`}
-    );`;
+    values.forEach((value, index) => {
+      if (index === values.length - 1) {
+        valueQuery.append(SQL`${value}) `);
+      } else {
+        valueQuery.append(SQL`${value}, `);
+      }
+    });
 
-    await pool.query(occurrenceQuery);
+    query.append(valueQuery);
+
+    if (i !== data.length - 1) {
+      query.append(SQL`, `);
+    }
   }
+
+  if (returning) {
+    query.append(` RETURNING ${returning};`);
+  } else {
+    query.append(";");
+  }
+
+  return query;
 };
 
-export const getStringDistance = (a: string, b: string) => {
-  const lv = new Levenshtein(a, b);
-  return lv.distance;
-};
-
-export const getStorage = async (init = true) => {
+export const getStorage = () => {
   const storage = new CodewatchPgStorage({
     user: process.env.POSTGRES_DB_USERNAME,
     host: process.env.POSTGRES_DB_HOST,
@@ -162,17 +103,6 @@ export const getStorage = async (init = true) => {
     password: process.env.POSTGRES_DB_PASSWORD,
     port: Number(process.env.POSTGRES_DB_PORT),
   });
-  if (init) await storage.init();
-  return storage;
-};
 
-export const withinDateRange = (
-  timestamp: string,
-  startDate: string,
-  endDate: string
-) => {
-  const start = new Date(startDate).getTime();
-  const end = new Date(endDate).getTime();
-  const current = new Date(timestamp).getTime();
-  return current >= start && current <= end;
+  return storage;
 };
